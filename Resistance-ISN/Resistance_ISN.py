b39d243697b1439dfbd017fc831d7925fe8ac0e0
Frédéric TRAN <frederictran@hotmail.fr>
2018-11-28 22:55:36 +0100

    Optimisation et ajout du bouton Effacer

--- Resistance-ISN/Resistance_ISN.py
+++ Resistance-ISN/Resistance_ISN.py
@@ -13,17 +13,18 @@ violet = 'violet'
 gris = 'grey'
 blanc = '#FFFFFF'
 
-Resis = 0
-
 RB = [0, 1, 2, 3]
 
 #Définition Fonction
 
 def Couleurs():
-	global Resis
-	Resis = ZT.get()
-	strResis = list(str(Resis))
+	strResis = list(str(ZT.get()))
 	for I in range(4):
+		if I == 3 and len(str(ZT.get())) > 3:
+			strResis[I] = str(len(ZT.get()) - 3)
+		if I ==3 and len(str(ZT.get())) <= 3:
+			Graphique.itemconfig(RB[I], fill = noir)
+			break;
 		if strResis[I] == "0":
 			Graphique.itemconfig(RB[I], fill = noir)
 		if strResis[I] == "1":
@@ -44,8 +45,12 @@ def Couleurs():
 			Graphique.itemconfig(RB[I], fill = gris)
 		if strResis[I] == "9":
 			Graphique.itemconfig(RB[I], fill = blanc)
-		if I >= 2:
-			strResis[3] = str(len(Resis) - 3)
+
+
+def Clean():
+	for I in range(4):
+		Graphique.itemconfig(RB[I], fill = blanc)
+	ZT.delete(first = 0, last = len(str(ZT.get())))
 
 #Création Fenètre
 Fenetre = Tk()
@@ -56,24 +61,28 @@ Graphique = Canvas(Fenetre, width = 1280, height = 720, bg = blanc)
 Graphique.pack()
 
 #Création Bouton Quitter
-BoutonQuitter = Button(Fenetre, text ='Quitter', command = Fenetre.destroy)
+BoutonQuitter = Button(Fenetre, text = 'Quitter', command = Fenetre.destroy)
 BoutonQuitter.pack(side = RIGHT, padx = 5, pady = 5)
 
 #Création Zone de Texte
 ZT = Entry()
 ZT.focus_set()
-ZT.pack(side = RIGHT, fill = BOTH) #Il faut le centrer entre les 2 boutons
+ZT.pack(side = LEFT, fill = BOTH) #Il faut le centrer entre les 2 boutons
 
 #Création Bouton Afficher
-BoutonAfficher = Button(Fenetre, text ='Afficher', command = Couleurs)
+BoutonAfficher = Button(Fenetre, text = 'Afficher', command = Couleurs)
 BoutonAfficher.pack(side = LEFT, padx = 5, pady = 5)
+
+#Création Bouton Effacer
+BoutonEffacer = Button(Fenetre, text = 'Effacer', command = Clean)
+BoutonEffacer.pack(side = LEFT)
 
 #Création Rectangle Central
 RC = Graphique.create_rectangle(160, 180, 1120, 540, outline = noir , fill = '#87591A')
 
 #Création Lignes
-LN = Graphique.create_line(20, 360, 160 , 360)
-LN2 = Graphique.create_line(1120, 360, 1260, 360)
+LN = Graphique.create_line(20, 360, 160 , 360, width = 20)
+LN2 = Graphique.create_line(1120, 360, 1260, 360, width = 20)
 
 #Création Bandes
 RB[0] = Graphique.create_rectangle(260, 180, 360, 540, fill = blanc)
