5d35ee1dcd01b0753d5629a9d79e770c85c40164
Frédéric TRAN <frederictran@hotmail.fr>
2018-12-01 00:36:13 +0100

    Ajout de texte

--- Resistance-ISN/Resistance_ISN.py
+++ Resistance-ISN/Resistance_ISN.py
@@ -4,16 +4,20 @@ from tkinter import *
 
 RB = [0, 1, 2, 3]
 
+Resis = 0
+
 #Définition des Couleurs (PC = Palette de Couleurs)
 #Dans l'ordre: noir, marron, rouge, orange, jaune, vert, bleu, violet, gris, blanc
 PC = ['#000000', '#582900', '#FF0000', 'orange', 'yellow', '#00FF00', '#0000FF', 'violet', 'grey', '#FFFFFF']
 
-#Définition Fonctions
 
+#Définition Fonctions
 #Fonction de coloration des bandes
 def Couleurs():
-	strResis = list(str(ZT.get()))
+	global strResis
+	strResis = str(ZT.get())
 	Clean()
+	Info()
 	if len(strResis) <= 3:
 		for I in range(len(strResis)):
 			Graphique.itemconfig(RB[I], fill = PC[int(strResis[I])])
@@ -27,8 +31,11 @@ def Clean():
 	for I in range(4):
 		Graphique.itemconfig(RB[I], fill = PC[0])
 	ZT.delete(first = 0, last = len(str(ZT.get())))
+	Resis.set("")
 
-#def Info(): (WIP)
+#Fonction d'affichage pour la couleur du résistance
+def Info():
+	Resis.set('Voici les couleurs du résistance pour: ' + strResis)
 
 
 #Création Fenètre
@@ -44,9 +51,9 @@ BoutonQuitter = Button(Fenetre, text = 'Quitter', command = Fenetre.destroy)
 BoutonQuitter.pack(side = RIGHT, padx = 5, pady = 5)
 
 #Création Zone de Texte
-ZT = Entry()
+ZT = Entry(justify = CENTER)
 ZT.focus_set()
-ZT.pack(side = LEFT, fill = BOTH) #Il faut le centrer entre les 2 boutons
+ZT.pack(side = LEFT, fill = BOTH)
 
 #Création Bouton Afficher
 BoutonAfficher = Button(Fenetre, text = 'Afficher', command = Couleurs)
@@ -56,14 +63,11 @@ BoutonAfficher.pack(side = LEFT, padx = 5, pady = 5)
 BoutonEffacer = Button(Fenetre, text = 'Effacer', command = Clean)
 BoutonEffacer.pack(side = LEFT)
 
-#Création Textes (WIP)
-#T1 = Text(Graphique)
-#T1.insert(INSERT, 'Voici les couleurs du résistance pour:')
-#T1.pack(side = TOP)
-
-#Resis = StringVar()
-#TA = Text(Fenetre)
-#TA.pack(side = TOP)
+#Création Texte
+#Texte annonce nombre
+Resis = StringVar()
+TA = Label(Fenetre, textvariable = Resis)
+TA.pack()
 
 #Création Rectangle Central
 RC = Graphique.create_rectangle(160, 180, 1120, 540, outline = PC[0] , fill = '#87591A')
