2b884269569ba539c35bf0286ccff0104729f68b
Frédéric TRAN <frederictran@hotmail.fr>
2018-12-04 21:45:35 +0100

    Ajout de la fonction de 3 bandes et correction du programme

--- Resistance-ISN/Resistance_ISN.py
+++ Resistance-ISN/Resistance_ISN.py
@@ -5,6 +5,7 @@ from tkinter.messagebox import *
 #Définition des variables
 
 RB = [0, 1, 2, 3]
+global ChiffresSigni
 
 #PHrase de début
 PHStart = "<--- Veuillez saisir la valeur de votre résistance"
@@ -19,29 +20,28 @@ PC = ['#000000', '#582900', '#FF0000', 'orange', 'yellow', '#00FF00', '#0000FF',
 #Fonction de vérification s'il n'y a pas de nombres décimaux et nombres > 10^9
 def Verification():
 	global strResis
-	strResis = str(ZT.get())
+	global ChiffresSigni
+	strResis = list(str(ZT.get()))
 	point = 0
 	for I in range(len(strResis)):
-		if strResis[I] == "." or strResis[I] == " " or len(strResis) == 0:
+		if strResis[I] == "." or strResis[I] == " ":
 			point = 1
 			break;
 	if len(strResis) > 10 or point == 1:
 		WARN = showwarning("Attention!", "Je digère mal les nombres décimaux, les espaces et les nombres au dessus de 10^9. Veuillez vérifier votre saisie")
 		Resis.set('<--- Veuillez vérifier votre valeur')
 	else:
-		Couleurs()
+		Couleurs(ChiffresSigni)
 
 #Fonction de coloration des bandes
-def Couleurs():
-	Clean()
-	if len(strResis) <= 3:
-		for I in range(len(strResis)):
-			Graphique.itemconfig(RB[I], fill = PC[int(strResis[I])])
-	else:
-		for I in range(3):
-			Graphique.itemconfig(RB[I], fill = PC[int(strResis[I])])
+def Couleurs(ChiffresSigni):
+	#Clean()
+	Resis.set('Voici les couleurs de la résistance pour: ' + "".join(strResis))
+	for I in range(ChiffresSigni):
+		Graphique.itemconfig(RB[I], fill = PC[int(strResis[I])])
+	del strResis[0:ChiffresSigni-1]
 	Graphique.itemconfig(RB[3], fill = PC[len(strResis)-1])
-	Resis.set('Voici les couleurs de la résistance pour: ' + strResis)
+
 
 #Fonction "Quand on appuie sur "enter""
 def Enter(event):
@@ -53,6 +53,15 @@ def Clean():
 		Graphique.itemconfig(RB[I], fill = PC[0])
 	ZT.delete(first = 0, last = len(str(ZT.get())))
 	Resis.set(PHStart)
+
+#Fonction du choix du nombres de bandes
+def Bandes4():
+	Graphique.itemconfig(RB[2], fill = PC[0], outline = PC[0])
+	ChiffresSigni = 3
+
+def Bandes3():
+	Graphique.itemconfig(RB[2], fill = "#87591A", outline = "#87591A")
+	ChiffresSigni = 2
 
 #Fonction Ouvrir la page du projet
 def Web():
@@ -73,6 +82,13 @@ menubar = Menu(Fenetre)
 filemenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label = "Fichier", menu = filemenu)
 filemenu.add_command(label = "Quitter", command = Fenetre.destroy)
+
+Actif = IntVar()
+ChiffresSigni = 3
+editmenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label = "Edition", menu = editmenu)
+editmenu.add_radiobutton(label = "Résistance à 4 bandes", variable = Actif, value = 0, command = Bandes4)
+editmenu.add_radiobutton(label = "Résistance à 3 bandes", variable = Actif, value = 1, command = Bandes3)
 
 helpmenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label = "Aide", menu = helpmenu)
@@ -109,7 +125,7 @@ RC = Graphique.create_rectangle(160, 120, 1120, 360, outline = PC[0] , fill = '#
 LN = Graphique.create_line(20, 240, 160 , 240, width = 20)
 LN2 = Graphique.create_line(1120, 240, 1260, 240, width = 20)
 
-#Création Bandes
+#Création Rectangles Bandes
 RB[0] = Graphique.create_rectangle(260, 120, 360, 360, fill = PC[0])
 RB[1] = Graphique.create_rectangle(410, 120, 510, 360, fill = PC[0])
 RB[2] = Graphique.create_rectangle(560, 120, 660, 360, fill = PC[0])
